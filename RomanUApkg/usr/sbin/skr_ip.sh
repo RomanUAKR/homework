c9fcb92d6f5ef67988945f75d586e3deb1737090
RomanUAKR <hatapribrana@gmail.com>
2023-12-17 20:41:08 +0000

    version 1.03 + skrin

--- RomanUApkg/usr/sbin/skr_ip.sh
+++ RomanUApkg/usr/sbin/skr_ip.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Отримати поточну дату і час у форматі року-місяця-дня години:хвилини:секунди
 DATETIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Отримати IP-адресу клієнта, який підключається до SSH (змінна $SSH_CONNECTION)
 IP=$(echo $SSH_CONNECTION | awk '{print $1}')
 
+# Якщо $IP порожній, спробуємо використати інший метод для отримання IP
 if [ -z "$IP" ]; then
+  # Визначити IP за допомогою команди last
   IP=$(last -i | grep "still logged in" | awk '{print $3}')
 fi
 
+# Перевірити, чи отримано значення IP
 if [ -n "$IP" ]; then
+  # Записати дату, час і IP-адресу до файлу client_log.txt
   echo "$DATETIME - Connected: $IP" >> /usr/sbin/client_log.txt
 else
-  if [ -s /usr/sbin/client_log.txt ]; then
-    echo "$DATETIME - Disconnected" >> /usr/sbin/client_log.txt
+  # Перевірити, чи не порожній файл (якщо він порожній - це відключення без попереднього підключення)
+  if [ -s /user/sbin/client_log.txt ]; then
+    echo "$DATETIME - Disconnected" >> /user/sbin/client_log.txt
   fi
 fi
-
-# Вивести унікальні записи "дата час конект IP"
-awk '!seen[$0]++' /usr/sbin/client_log.txt | awk '{if (NF>1) {for (i=2; i<=NF; i++) {print $1, $2, $3, " Connected:", $i; echo "  дата час  конект " $i, $1, $2, $3}}}' >> /usr/sbin/unique_client_log.txt
